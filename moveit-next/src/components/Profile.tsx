496245f904002f781495b4d3c2102ee3aafe1ada
Igor Delesposti <igordelesposti@gmail.com>
2021-03-10 14:14:22 -0300

    add estructure leveling and reset countdown when failed or success

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/igordelesposti.png" alt="Igor Delesposti" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Igor Delesposti</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-            Level 1
+            Level {level}
         </p>
       </div>
     </div>
